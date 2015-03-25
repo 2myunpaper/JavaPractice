3578bb57f5f7a6bf620c27b294be18b1d4dbcf74
wp132304 <dhk0420@naver.com>
2015-03-25 14:00:11 +0900

    Update Condition.java

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -3,8 +3,8 @@ package org.dimigo.basic;
 public class Condition {
 	public static void main(String[] args) {
 		
-		String carsType = "그 외";
-		int kilometer = 25;
+		String carsType = "경차";
+		int kilometer = 10;
 		int fare;
 		
 		switch(carsType) {
@@ -18,19 +18,19 @@ public class Condition {
 			fare = 600;
 		}
 		
-		if(kilometer / 10 > 1) {
+		if(kilometer > 10) {
 			switch(carsType) {
 			case "고속버스":
-				fare += (kilometer / 10 - 1) * 300;
+				fare += (kilometer / 10) * 300;
 				break;
 			default:
-				fare += (kilometer / 10 - 1) * 200;
+				fare += (kilometer / 10) * 200;
 			}
 		}
 		
 		System.out.println("<< 고속도로 주행 비용 >>");
-		System.out.println("차 종류 : " + carsType);
 		System.out.println("주행 거리 : " + kilometer + "km");
+		System.out.println("차 종류 : " + carsType);
 		System.out.println("주행 비용 : " + fare + "원");
 		
 	}
