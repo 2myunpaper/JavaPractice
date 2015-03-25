06761e86a9688a29a932a1502c5d94e5ec01a836
eastl <eastl@192.168.109.1>
2015-03-25 14:04:48 +0900

    Promotion

--- JavaPractice/src/org/dimigo/basic/Promotion.java
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -10,6 +10,7 @@ public class Promotion {
 		int salary = 1700000;
 		long allSalary = (long)person * (long)marketNum * (long)salary * 12;
 		
+		
 		System.out.println("<< 디미베네 연간 인건비 >>");
 		System.out.printf("월 평균 급여 : %,d원\n", salary);
 		System.out.printf("점포 내 직원 수 : %,d명\n", person);
